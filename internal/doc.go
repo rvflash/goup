1597faf1c9cfc885e1cb4f9580a25785fe0f305d
hgouchet <rvflash@users.noreply.github.com>
2020-02-21 20:47:42 +0100

    codecov test

---
+++ internal/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2020 Hervé Gouchet. All rights reserved.
+// Use of this source code is governed by the MIT License
+// that can be found in the LICENSE file.
+
+// Package internal lists the packages internally used by the go up application.
+package internal
