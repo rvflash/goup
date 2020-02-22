f5d83230938f6db6d269d1921ea79966dad01163
hgouchet <rvflash@users.noreply.github.com>
2020-02-22 14:40:57 +0100

    real test coverage

--- pkg/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2020 Hervé Gouchet. All rights reserved.
-// Use of this source code is governed by the MIT License
-// that can be found in the LICENSE file.
-
-// Package pkg contains the public API of the go up tool.
-package pkg
